c983eb9a5aa0944b14bcd91c327450300cef6e41
m2alpha <58631850+m2alpha@users.noreply.github.com>
2020-10-31 21:30:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,14 +11,6 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-    
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if ctx.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if ctx.content == '/panda':
-        await ctx.channel.send('ぶぅもぁああああ！ぱんだ～')
 
 @bot.command()
 async def ping(ctx):
