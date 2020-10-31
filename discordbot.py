f023fe595d55a5db5e48beb98fd2f8cc912ce6b2
m2alpha <58631850+m2alpha@users.noreply.github.com>
2020-11-01 00:58:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,36 +1,20 @@
-from discord.ext import commands
+import discord
 import os
-import traceback
-#import discord
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
-#TEXT_CHANNEL = 767123336869576728
+TOKEN = os.environ['DISCORD_BOT_TOKEN'] # トークンキー
+TEXT_CHANNEL = 767123336869576728 # テキストチャットのチャンネルID
 
-#text_chat = discord.Object(id=TEXT_CHANNEL)
-#bot2 = commands.Bot
-#@bot2.event
-#async def on_message(message):
-#    if message.content.startswith("こんぱんだ"):
-#        if bot2.user != message.author:
-#            msg = "こんぱんだ " + message.author.name + "さん！"
-#            await bot2.send_message(message.channel, msg)
+client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+text_chat = discord.Object(id=TEXT_CHANNEL)
 
-@bot.event
+# こんにちはメッセージ
+
+@client.event
 async def on_message(message):
-    if message.content.startswith("こんぱんだ"):
-        if bot.user != message.author:
-            msg = "こんぱんだ " + message.author.name + "さん！"
-            await bot.send_message(message.channel, msg)
-    
-@bot.command()
-async def panda(ctx):
-    await ctx.send('ぶぅもぁああああ！ぱんだ～')
+    if message.content.startswith("こんにちは"):
+        if client.user != message.author:
+            msg = "こんにちは " + message.author.name + "さん！"
+            await client.send_message(message.channel, msg)
 
-bot.run(token)
+client.run(TOKEN)
