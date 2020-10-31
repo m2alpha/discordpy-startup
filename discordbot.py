adc4361a43a2d076a7582c3a61b5808f836818aa
m2alpha <58631850+m2alpha@users.noreply.github.com>
2020-11-01 00:27:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,21 @@
 from discord.ext import commands
 import os
 import traceback
+import discord
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+TEXT_CHANNEL = 767123336869576728
 
+text_chat = discord.Object(id=TEXT_CHANNEL)
+bot2 = commands.Bot
+
+@bot2.event
+async def on_message(message):
+    if message.content.startswith("こんぱんだ"):
+        if bot2.user != message.author:
+            msg = "こんぱんだ " + message.author.name + "さん！"
+            await bot2.send_message(message.channel, msg)
 
 @bot.event
 async def on_command_error(ctx, error):
